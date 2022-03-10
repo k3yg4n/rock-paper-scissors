a2c5bec4a5bc0bfc38cfe1e97a7641eb9e5c801f
Keegan Liu <keeganliu5678@gmail.com>
2022-03-09 22:19:12 -0500

    Reworked playRound function to display the result and return a string depending on the winner. Also implemented the output for the final winner of the five rounds.

--- javascript.js
+++ javascript.js
@@ -14,32 +14,59 @@ function playRound(playerSelection, computerSelection){
     console.log("PLAYER: " + playerSelection);
     console.log("COM: " + computerSelection);
 
+    let winner = "";
+
     if((playerSelection === "rock") && (computerSelection === "scissors")){
-        return "You win! Rock beats scissors!";
+        console.log("You win! Rock beats scissors!");
+        winner = "player";
     } else if ((playerSelection === "rock") && (computerSelection === "paper")){
-        return "You lose! Paper beats rock!";
+        console.log("You lose! Paper beats rock!");
+        winner = "computer";
     } else if ((playerSelection === "scissors") && (computerSelection === "rock")){
-        return "You lose! Rock beats scissors!";
+        console.log("You lose! Rock beats scissors!")
+        winner = "computer";
     } else if ((playerSelection === "scissors") && (computerSelection === "paper")){
-        return "You win! Scissors beats paper!";
+        console.log("You win! Scissors beats paper!");
+        winner = "player";
     } else if ((playerSelection === "paper") && (computerSelection === "rock")){
-        return "You win! Paper beats rocks!";
+        console.log("You win! Paper beats rocks!");
+        winner = "player";
     } else if ((playerSelection === "paper") && (computerSelection === "scissors")){
-        return "You lose! Scissors beats paper!";
+        console.log("You lose! Scissors beats paper!");
+        winner = "computer";
     } else {
-        return "Tie! Both players selected the same item!"
+        console.log("Tie! Both players selected the same item!");
+        winner = "none";
     }
-    
+
+    return winner;
 }
 
-
 function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    let winner = "";
+
     for(let i = 0; i < 5; i++){
         playerSelection = prompt("Please enter your choice (rock,paper, or scissors)").toLowerCase();
         computerSelection = computerPlay();
-        console.log(playRound(playerSelection,computerSelection));
+        winner = playRound(playerSelection,computerSelection);
+        if(winner === "player"){
+            playerScore++;
+        } else if (winner === "computer") {
+            computerScore++;
+        }
+    }
+
+    if(playerScore > computerScore){
+        console.log(`The PLAYER wins! The final score is ${playerScore}-${computerScore}.`);
+    } else if(computerScore > playerScore) {
+        console.log(`The COMPUTER wins! The final score is ${computerScore}-${playerScore}.`);
+    } else {
+        console.log(`The game was a tie with a final score of ${computerScore}-${playerScore}.`);
     }
 }
 
 // console.log(playRound(playerSelection,computerSelection)); // To test first 2 funcs
+
 game();
